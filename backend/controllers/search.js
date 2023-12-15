9c3555f902a9733da8eb0a583b77715377c5d3ba
namdeft <dinhnam2306@gmail.com>
2023-12-15 22:53:53 +0700

    refactor code

--- backend/controllers/search.js
+++ backend/controllers/search.js
@@ -1,53 +1,45 @@
 import { db } from "../connect.js";
 
+const baseQuery = `
+  SELECT
+    dish.*,
+    GROUP_CONCAT(DISTINCT ingredient.name) as ingredients,
+    GROUP_CONCAT(DISTINCT origin.name) as origins,
+    GROUP_CONCAT(DISTINCT category.name) as categories
+  FROM dish
+    LEFT JOIN dish_ingredient ON dish.ID = dish_ingredient.dish_id
+    LEFT JOIN ingredient ON dish_ingredient.ingredient_id = ingredient.ID
+    LEFT JOIN dish_origin ON dish.ID = dish_origin.dish_id
+    LEFT JOIN origin ON dish_origin.origin_id = origin.ID
+    LEFT JOIN dish_category ON dish.ID = dish_category.dish_id
+    LEFT JOIN category ON dish_category.category_id = category.ID
+`;
+
+const getSearchResults = (req, res, searchParam, searchType) => {
+  const query = `${baseQuery} WHERE ${searchType}.name LIKE ? GROUP BY dish.ID`;
+
+  db.query(query, [searchParam + '%'], (err, data) => {
+    if (err) return res.status(500).json(err);
+    res.status(200).json({ data });
+  });
+};
+
 export const getSearchDish = (req, res) => {
-  const dishName = req.params.searchDish
-
-  const query = "SELECT dish.*, GROUP_CONCAT(DISTINCT ingredient.name) as ingredients, GROUP_CONCAT(DISTINCT origin.name) as origins, GROUP_CONCAT(DISTINCT category.name) as categories FROM dish LEFT JOIN dish_ingredient ON dish.ID = dish_ingredient.dish_id LEFT JOIN ingredient ON dish_ingredient.ingredient_id = ingredient.ID LEFT JOIN dish_origin ON dish.ID = dish_origin.dish_id LEFT JOIN origin ON dish_origin.origin_id = origin.ID LEFT JOIN dish_category ON dish.ID = dish_category.dish_id LEFT JOIN category ON dish_category.category_id = category.ID WHERE dish.name LIKE ? GROUP BY dish.ID"
-
-  db.query(query, [dishName + '%'], (err, data) => {
-    if (err) return err;
-    return res.status(200).json({
-      data,
-    })
-  })
-}
+  const dishName = req.params.searchDish;
+  getSearchResults(req, res, dishName, 'dish');
+};
 
 export const getSearchCategory = (req, res) => {
-  const categoryName = req.params.searchCategory
-
-  const query = "SELECT dish.*, GROUP_CONCAT(DISTINCT ingredient.name) as ingredients, GROUP_CONCAT(DISTINCT origin.name) as origins, GROUP_CONCAT(DISTINCT category.name) as categories FROM dish LEFT JOIN dish_ingredient ON dish.ID = dish_ingredient.dish_id LEFT JOIN ingredient ON dish_ingredient.ingredient_id = ingredient.ID LEFT JOIN dish_origin ON dish.ID = dish_origin.dish_id LEFT JOIN origin ON dish_origin.origin_id = origin.ID LEFT JOIN dish_category ON dish.ID = dish_category.dish_id LEFT JOIN category ON dish_category.category_id = category.ID WHERE category.name LIKE ? GROUP BY dish.ID"
-
-  db.query(query, [categoryName + '%'], (err, data) => {
-    if (err) return err;
-    return res.status(200).json({
-      data,
-    })
-  })
-}
+  const categoryName = req.params.searchCategory;
+  getSearchResults(req, res, categoryName, 'category');
+};
 
 export const getSearchOrigin = (req, res) => {
-  const originName = req.params.searchOrigin
-
-  const query = "SELECT dish.*, GROUP_CONCAT(DISTINCT ingredient.name) as ingredients, GROUP_CONCAT(DISTINCT origin.name) as origins, GROUP_CONCAT(DISTINCT category.name) as categories FROM dish LEFT JOIN dish_ingredient ON dish.ID = dish_ingredient.dish_id LEFT JOIN ingredient ON dish_ingredient.ingredient_id = ingredient.ID LEFT JOIN dish_origin ON dish.ID = dish_origin.dish_id LEFT JOIN origin ON dish_origin.origin_id = origin.ID LEFT JOIN dish_category ON dish.ID = dish_category.dish_id LEFT JOIN category ON dish_category.category_id = category.ID WHERE origin.name LIKE ? GROUP BY dish.ID"
-
-  db.query(query, [originName + '%'], (err, data) => {
-    if (err) return err;
-    return res.status(200).json({
-      data,
-    })
-  })
-}
+  const originName = req.params.searchOrigin;
+  getSearchResults(req, res, originName, 'origin');
+};
 
 export const getSearchIngredient = (req, res) => {
-  const ingredientName = req.params.searchIngredient
-
-  const query = "SELECT dish.*, GROUP_CONCAT(DISTINCT ingredient.name) as ingredients, GROUP_CONCAT(DISTINCT origin.name) as origins, GROUP_CONCAT(DISTINCT category.name) as categories FROM dish LEFT JOIN dish_ingredient ON dish.ID = dish_ingredient.dish_id LEFT JOIN ingredient ON dish_ingredient.ingredient_id = ingredient.ID LEFT JOIN dish_origin ON dish.ID = dish_origin.dish_id LEFT JOIN origin ON dish_origin.origin_id = origin.ID LEFT JOIN dish_category ON dish.ID = dish_category.dish_id LEFT JOIN category ON dish_category.category_id = category.ID WHERE ingredient.name LIKE ? GROUP BY dish.ID"
-
-  db.query(query, [ingredientName + '%'], (err, data) => {
-    if (err) return err;
-    return res.status(200).json({
-      data,
-    })
-  })
-}
+  const ingredientName = req.params.searchIngredient;
+  getSearchResults(req, res, ingredientName, 'ingredient');
+};
